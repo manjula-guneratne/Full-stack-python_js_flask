80d44a2b5c6db0e8806405c772af2531a8de2d48
Manjula Guneratne <mguneratne3@gmail.com>
2024-02-23 22:32:00 -0500

    Modal added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import ContactForm from "./ContactForm";
 
 function App() {
   const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModelOpen] = useState(false);
 
   useEffect(() => {
     fetchContacts();
@@ -17,10 +18,27 @@ function App() {
     console.log(data.contacts);
   };
 
+  const closeModal = () => {
+    setIsModelOpen(false);
+  };
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModelOpen(true);
+  };
+
   return (
     <>
       <ContactList contacts={contacts} />
-      <ContactForm />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <ContactForm />
+          </div>
+        </div>
+      }
     </>
   );
 }
